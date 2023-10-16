e1c9f1f462a48eacc400b00ddfd8b83860dfa1f3
ebharathi <elayabharathi10102002@gmail.com>
2023-10-16 23:36:03 +0530

    connected service with frontend

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { BackendService } from '../backend.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
+  constructor(private service:BackendService){}
+  public languages:any=[];
+   ngOnInit()
+   {
+      this.service.getLanguages().subscribe((data:any)=>{
+           console.log("[+]Language --->",data);
+           if(data.data)
+           {
+            this.languages=data.data.languages;
+           }
+      })
+   }
 }
